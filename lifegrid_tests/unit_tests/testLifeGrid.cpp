722db298a580cc1d37541c6d4fbb6c6bda8de5e9
Anthony Clifton <anthony@changedearth.com>
2017-11-05 03:57:30 -0600

    Fixed another test

--- lifegrid_tests/unit_tests/testLifeGrid.cpp
+++ lifegrid_tests/unit_tests/testLifeGrid.cpp
@@ -12,8 +12,6 @@ protected:
     }
 
     virtual void TearDown() {
-        // Code here will be called immediately after each test
-        // (right before the destructor).
     }
 };
 
@@ -22,26 +20,22 @@ TEST(lifegrid, update_method_kills_lonely_cells) {
             {Cell::Cell(0,0,true)},
             {Cell::Cell(1,0,false)}
     };
-    
+
     LifeGrid lifeGrid;
     std::vector<std::vector<Cell>> updated_grid = lifeGrid.update(starting_grid);
     EXPECT_EQ(true, updated_grid[0][0].isDead());
 }
 
-//TEST(lifegrid, update_method_leaves_happy_cells_with_2_neighbors_alive) {
-//    std::vector<std::vector<int>> starting_grid = {
-//            {1, 0},
-//            {1, 1}
-//    };
-//    std::vector<std::vector<int>> expected_grid = {
-//            {1, 1},
-//            {1, 1}
-//    };
-//    LifeGrid lifeGrid;
-//    std::vector<std::vector<int>> updated_grid = lifeGrid.update(starting_grid);
-//    EXPECT_EQ(expected_grid, updated_grid);
-//}
-//
+TEST(lifegrid, update_method_leaves_happy_cells_with_2_neighbors_alive) {
+    std::vector<std::vector<Cell>> starting_grid = {
+            {Cell::Cell(0,0,true), Cell::Cell(1,0,false)},
+            {Cell::Cell(0,1,true), Cell::Cell(1,1,true)}
+    };
+    LifeGrid lifeGrid;
+    std::vector<std::vector<Cell>> updated_grid = lifeGrid.update(starting_grid);
+    EXPECT_EQ(true, updated_grid[0][1].isAlive());
+}
+
 //TEST(lifegrid, update_method_leaves_happy_cells_with_3_neighbors_alive) {
 //    std::vector<std::vector<int>> starting_grid = {
 //            {1, 1},
