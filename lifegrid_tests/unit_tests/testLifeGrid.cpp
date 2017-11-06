b47690aa63500a45441d6e07a36ca3da3ca7fcb6
Anthony Clifton <anthony@changedearth.com>
2017-11-05 19:01:52 -0600

    Restored all testLifeGrid tests

--- lifegrid_tests/unit_tests/testLifeGrid.cpp
+++ lifegrid_tests/unit_tests/testLifeGrid.cpp
@@ -60,88 +60,35 @@ TEST(lifegrid, update_method_kills_cells_with_4_neighbors) {
     EXPECT_EQ(false, updated_grid[1][1].isAlive());
 }
 
-//TEST(lifegrid, update_method_experiment_1) {
-//    std::vector<std::vector<int>> starting_grid = {
-//            {0, 0},
-//            {1, 0}
-//    };
-//    std::vector<std::vector<int>> expected_grid = {
-//            {0, 0},
-//            {0, 0}
-//    };
-//    LifeGrid lifeGrid;
-//    std::vector<std::vector<int>> updated_grid = lifeGrid.update(starting_grid);
-//    EXPECT_EQ(expected_grid, updated_grid);
-//}
-//
-//TEST(lifegrid, update_method_does_first_glider_step) {
-//    std::vector<std::vector<int>> starting_grid = {
-//            {0, 0, 0, 0},
-//            {1, 0, 0, 0},
-//            {0, 1, 1, 0},
-//            {1, 1, 0, 0}
-//    };
-//    std::vector<std::vector<int>> expected_grid = {
-//            {0, 0, 0, 0},
-//            {0, 1, 0, 0},
-//            {0, 0, 1, 0},
-//            {1, 1, 1, 0}
-//    };
-//    LifeGrid lifeGrid;
-//    std::vector<std::vector<int>> updated_grid = lifeGrid.update(starting_grid);
-//    EXPECT_EQ(expected_grid, updated_grid);
-//}
+TEST(lifegrid, update_method_generates_new_cells) {
+    std::vector<std::vector<Cell>> starting_grid = {
+            {Cell::Cell(0,0,true), Cell::Cell(1,0,true)},
+            {Cell::Cell(0,1,true), Cell::Cell(1,1,false)}
+    };
+    LifeGrid lifeGrid;
+    std::vector<std::vector<Cell>> updated_grid = lifeGrid.update(starting_grid);
+    EXPECT_EQ(true, updated_grid[1][1].isAlive());
+}
 
-//TEST(lifegrid, update_method_kills_overpopulated_cells) {
-//    std::vector<std::vector<int>> starting_grid = {
-//            {1, 1, 1},
-//            {1, 0, 1}
-//    };
-//    std::vector<std::vector<int>> expected_grid = {
-//            {1, 0, 1},
-//            {1, 0, 1}
-//    };
-//    LifeGrid lifeGrid;
-//    lifeGrid.grid = starting_grid;
-//    std::vector<std::vector<int>> updated_grid = lifeGrid.update();
-//    EXPECT_EQ(expected_grid, updated_grid);
-//}
-//
-//TEST(lifegrid, update_method_generates_new_cells) {
-//    std::vector<std::vector<int>> starting_grid = {
-//            {1, 1},
-//            {1, 0}
-//    };
-//    std::vector<std::vector<int>> expected_grid = {
-//            {1, 1},
-//            {1, 1}
-//    };
-//    LifeGrid lifeGrid;
-//    lifeGrid.grid = starting_grid;
-//    std::vector<std::vector<int>> updated_grid = lifeGrid.update();
-//    EXPECT_EQ(expected_grid, updated_grid);
-//}
-//
-//TEST(lifegrid, count_neighbors_returns_0_when_no_neighbors) {
-//    std::vector<std::vector<int>> current_grid = {{1}};
-//    LifeGrid lifeGrid;
-//    lifeGrid.grid = current_grid;
-//    int neighbors = lifeGrid.countNeighbors(0,0);
-//    EXPECT_EQ(0, neighbors);
-//}
-//
-//TEST(lifegrid, count_neighbors_returns_1_when_one_neighbor) {
-//    std::vector<std::vector<int>> current_grid = {{1, 1}};
-//    LifeGrid lifeGrid;
-//    lifeGrid.grid = current_grid;
-//    int neighbors = lifeGrid.countNeighbors(0,0);
-//    EXPECT_EQ(1, neighbors);
-//}
-//
-//TEST(lifegrid, count_neighbors_returns_2_when_one_neighbor) {
-//    std::vector<std::vector<int>> current_grid = {{1, 1}, {1, 0}};
-//    LifeGrid lifeGrid;
-//    lifeGrid.grid = current_grid;
-//    int neighbors = lifeGrid.countNeighbors(0,0);
-//    EXPECT_EQ(2, neighbors);
-//}
+TEST(lifegrid, count_neighbors_returns_0_when_no_neighbors) {
+    std::vector<std::vector<Cell>> current_grid = {{Cell::Cell(0,0,true)}};
+    LifeGrid lifeGrid;
+    int neighbors = lifeGrid.countNeighbors(current_grid[0][0], current_grid);
+    EXPECT_EQ(0, neighbors);
+}
+
+TEST(lifegrid, count_neighbors_returns_1_when_one_neighbor) {
+    std::vector<std::vector<Cell>> current_grid = {{Cell::Cell(0,0,true), Cell::Cell(1,0,true)}};
+    LifeGrid lifeGrid;
+    int neighbors = lifeGrid.countNeighbors(current_grid[0][0], current_grid);
+    EXPECT_EQ(1, neighbors);
+}
+
+TEST(lifegrid, count_neighbors_returns_2_when_one_neighbor) {
+    std::vector<std::vector<Cell>> current_grid = {
+            {Cell::Cell(0,0,true), Cell::Cell(1,0,true)},
+            {Cell::Cell(0,1,true), Cell::Cell(1,1,false)}};
+    LifeGrid lifeGrid;
+    int neighbors = lifeGrid.countNeighbors(current_grid[0][0], current_grid);
+    EXPECT_EQ(2, neighbors);
+}
